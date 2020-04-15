54ebf5bee006895d63bd4e9c8d2a26758d160452
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-04-14 23:37:52 -0700

    added handlebars

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs  = require('express-handlebars');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
