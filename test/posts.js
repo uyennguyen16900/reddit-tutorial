29b4a24013fdc165efa31326b7e483fc797dd32f
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-04-30 23:50:05 -0700

    fixed posts test

--- test/posts.js
+++ test/posts.js
@@ -9,6 +9,8 @@ const expect = chai.expect;
 const Post = require('../models/Post');
 const server = require('../server');
 
+const agent = chai.request.agent(app);
+
 chai.should();
 chai.use(chaiHttp);
 
@@ -20,7 +22,78 @@ describe('Posts', function() {
       url: 'https://www.google.com',
       summary: 'post summary'
   };
+
+  const user = {
+    username: 'poststest',
+    password: 'testposts'
+  };
+
+  before(function (done) {
+    agent
+      .post('/sign-up')
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send(user)
+      .then(function (res) {
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
+
+  after(function (done) {
+    Post.findOneAndDelete(newPost)
+    .then(function (res) {
+        agent.close()
+
+        User.findOneAndDelete({
+            username: user.username
+        })
+          .then(function (res) {
+              done()
+          })
+          .catch(function (err) {
+              done(err);
+          });
+    })
+    .catch(function (err) {
+        done(err);
+    });
+  });
+
   it("should create with valid attributes at POST /posts/new", function (done) {
-    // TODO: test code goes here!
+    // TODO: test code goes here
+    // Checks how many posts there are now
+      Post.estimatedDocumentCount()
+      .then(function (initialDocCount) {
+        chai
+          .request(server)
+          .post("/posts/new")
+          // This line fakes a form post,
+          // since we're not actually filling out a form
+          .set("content-type", "application/x-www-form-urlencoded")
+          // Make a request to create another
+          .send(newPost)
+          .then(function (res) {
+              Post.estimatedDocumentCount()
+                  .then(function (newDocCount) {
+                      // Check that the database has one more post in it
+                      expect(res).to.have.status(200);
+                      // Check that the database has one more post in it
+                      expect(newDocCount).to.be.equal(initialDocCount + 1)
+                      done();
+                  })
+                  .catch(function (err) {
+                      done(err);
+                  });
+          })
+          .catch(function (err) {
+              done(err);
+          });
+    })
+    .catch(function (err) {
+        done(err);
+    });
+
   });
 });
